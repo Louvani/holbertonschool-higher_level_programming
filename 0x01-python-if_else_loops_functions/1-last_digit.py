3afe8dcb7ebe18664cd942b9d58a1d71e442e07b
Louvani <paula.louvani@gmail.com>
2021-04-27 13:13:31 -0500

    Task 1 print negative numbers and pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+if number > 0:
+    last_digit = number % 10
+elif number < 0:
+    number = number * -1
+    last_digit = number % 10
+    last_digit = last_digit * -1
+    number = number * -1
+
 if last_digit > 5:
-    print("Last digit of {:d} is {: d} and is greater than \
-5".format(number, last_digit))
-if last_digit == 0:
-    print("Last digit of {:d} is {} and is \
-0".format(number, last_digit))
-if last_digit < 6:
-    print("Last digit of {:d} is {} and is less than \
-6 and not 0".format(number, last_digit))
+    print("Last digit of {:d} is {} and is greater than 5\
+".format(number, last_digit))
+elif last_digit == 0:
+    print("Last digit of {:d} is {} and is 0".format(number, last_digit))
+elif last_digit < 6:
+    print("Last digit of {:d} is {} and is less than 6 and not 0\
+".format(number, last_digit))
