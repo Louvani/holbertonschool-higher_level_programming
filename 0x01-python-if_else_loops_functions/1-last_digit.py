244bf7a5a83f168970b34b4489d9c735df5c59f4
Louvani <paula.louvani@gmail.com>
2021-04-27 13:29:09 -0500

    Revert "Train more quotes for lenght of a line"
    This reverts commit ed0f6fd2d3c72f263efc641d83502d48aeab3843.

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,10 +10,10 @@ elif number < 0:
     number = number * -1
 
 if last_digit > 5:
-    print("Last digit of {:d} is {}"
-            " and is greater than 5".format(number, last_digit))
+    print("Last digit of {:d} is {} and is greater than 5\
+".format(number, last_digit))
 elif last_digit == 0:
     print("Last digit of {:d} is {} and is 0".format(number, last_digit))
 elif last_digit < 6:
-    print("Last digit of {:d} is {}"
-            " and is less than 6 and not 0".format(number, last_digit))
+    print("Last digit of {:d} is {} and is less than 6 and not 0\
+".format(number, last_digit))
