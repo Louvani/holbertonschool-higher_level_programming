8331f6db857f4dffd980a1991b412b615c3a750d
Louvani <paula.louvani@gmail.com>
2021-04-27 15:43:18 -0500

    Task1 divede the sentences

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-str1 = "and is greater than 5"
-str2 = "and is less than 6 and not 0"
-
 if number > 0:
-    last_digit = number % 10
+    digit = number % 10
 elif number < 0:
-    number = number * -1
-    last_digit = number % 10
-    last_digit = last_digit * -1
-    number = number * -1
-
-if last_digit > 5:
-    print("Last digit of {:d} is {:d} {}".format(number, last_digit, str1))
-elif last_digit == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, last_digit))
+    digit = number % -10
+print('Last digit of {:d} is {:d} '.format(number, digit), end="")
+if digit > 5:
+    print("and is greater than 5")
+elif digit == 0:
+    print("and is 0")
 else:
-    print("Last digit of {:d} is {:d} {}".format(number, last_digit, str2))
+    print("and is less than 6 and not 0")
