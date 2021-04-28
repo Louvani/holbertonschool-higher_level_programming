cb0179013c7e3123fd0c9bc5c048efac4488ac5f
Louvani <paula.louvani@gmail.com>
2021-04-28 13:52:59 -0500

    task0 if __name__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import add_0 as modd
-a = 1
-b = 2
-print("{} + {} = {}" .format(a, b, modd.add(a, b)))
+if __name__ == '__main__'
+    import add_0 as modd
+    a = 1
+    b = 2
+    print("{} + {} = {}" .format(a, b, modd.add(a, b)))
