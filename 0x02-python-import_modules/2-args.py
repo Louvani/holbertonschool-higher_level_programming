12bb0356cfc49ca660e1ca8d31eeb474e7af733a
Louvani <paula.louvani@gmail.com>
2021-04-28 17:49:03 -0500

    Task2 text for only one argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,11 @@
 if __name__ == '__main__':
     from sys import argv
     leng = len(argv)
-    print("0 arguments." if leng == 1 else "{} arguments:".format(leng - 1))
+    if leng == 1:
+        print("0 arguments.")
+    elif leng == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(leng - 1))
     for i in range(1, len(argv)):
         print("{}: {}".format(i, argv[i]))
