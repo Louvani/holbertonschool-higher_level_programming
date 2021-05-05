635e6cf902ea4f024c5533262270115d01dda22f
Louvani <paula.louvani@gmail.com>
2021-05-05 14:17:18 -0500

    Task2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return None
+    if idx > len(my_list):
+        return None
+    my_list[idx] = element
+    return my_list
