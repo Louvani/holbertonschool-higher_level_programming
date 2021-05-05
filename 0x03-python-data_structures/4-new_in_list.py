d057b94fb9f5bdaaa96fb1b561414dd6a662fa8b
Louvani <paula.louvani@gmail.com>
2021-05-05 17:33:31 -0500

    Task 4 idx +1

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     aux = my_list.copy()
     if idx < 0:
         return aux
-    if idx > (len(my_list)):
+    if idx + 1 > (len(my_list)):
         return aux
     aux[idx] = element
     return aux
