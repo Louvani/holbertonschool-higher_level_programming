34cbc6af338d0e3a64ef67acae3366dbedb527cd
Louvani <paula.louvani@gmail.com>
2021-05-05 18:50:23 -0500

    Task6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{}".format(matrix[i][j],), end="")
+            if j + 1 < len(matrix[i]):
+                print(end=" ")
+        print()
