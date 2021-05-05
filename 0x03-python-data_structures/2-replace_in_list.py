104b7fa79e55e4dceb151da3ae31495449a8cc13
Louvani <paula.louvani@gmail.com>
2021-05-05 17:31:53 -0500

    Task2 return my list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return None
+        return my_list
     if idx + 1 > len(my_list):
-        return None
+        return my_list
     my_list[idx] = element
     return my_list
