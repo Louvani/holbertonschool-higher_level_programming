93ddee9bdf24c0a7277df92f0153ec4b7b8848fb
Louvani <paula.louvani@gmail.com>
2021-05-05 17:29:46 -0500

    Task2 idx +1

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return None
-    if idx > len(my_list):
+    if idx + 1 > len(my_list):
         return None
     my_list[idx] = element
     return my_list
