ecb4cccacc01da2abced6e9cf0187400c271d83b
Louvani <paula.louvani@gmail.com>
2021-05-06 14:32:03 -0500

    0x04 task0 using map and lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[x ** 2 for x in matrix[i]]for i in range(len(matrix))]
+    new_matrix = [list(map(lambda x: x ** 2, num)) for num in matrix]
     return new_matrix
