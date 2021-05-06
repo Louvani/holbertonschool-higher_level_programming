eb018ea9c2bb5ece012abb30d680fe2cc8bafd3e
Louvani <paula.louvani@gmail.com>
2021-05-06 18:40:55 -0500

    0x04 task7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        dict1 = {key:value}
+        a_dictionary.update(dict1)
+    return a_dictionary
