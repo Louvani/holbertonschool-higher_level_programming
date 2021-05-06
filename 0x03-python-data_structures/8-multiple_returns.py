c1bdcc4095117a867445bf18dfe7b7589b7d0df2
Louvani <paula.louvani@gmail.com>
2021-05-05 22:08:13 -0500

    Task8 if sentence empty == none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     len_str = len(sentence)
-    first_char = sentence[0]
+    if len(sentence) == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
     t_tupple = len_str, first_char
     return t_tupple
