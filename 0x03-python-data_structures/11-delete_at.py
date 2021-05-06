46bbcf55b6302e3e600ed25b8dd13cd23b8a32d7
Louvani <paula.louvani@gmail.com>
2021-05-05 22:54:59 -0500

    Task11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if len(my_list) == 0:
-        return None
-    if idx < 0:
+    if idx < 0 or idx + 1 > len(my_list):
         return my_list
     del my_list[idx]
     return my_list
