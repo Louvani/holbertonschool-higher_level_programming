65b2fa47ddacf08287b8d74842975f18ee43b186
Louvani <paula.louvani@gmail.com>
2021-05-06 18:41:52 -0500

    0x04 task7 white space after :

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,6 +3,6 @@ def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
         a_dictionary[key] = value
     else:
-        dict1 = {key:value}
+        dict1 = {key: value}
         a_dictionary.update(dict1)
     return a_dictionary
