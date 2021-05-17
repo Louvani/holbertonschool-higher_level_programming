b9a34f55d9b99563b7688394d1b4a0317355a509
Louvani <paula.louvani@gmail.com>
2021-05-17 12:59:58 -0500

    0x05 task 0 cumber sum apart of the iteration

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,8 +2,9 @@
 def safe_print_list(my_list=[], x=0):
 	num = 0
 	try:
-		for num in range(0, x):
-			print("{:d}".format(my_list[num]), end="")
+		for i in range(0, x):
+			print("{:d}".format(my_list[i]), end="")
+			num += 1
 	except IndexError:
 		pass
 	print("")
