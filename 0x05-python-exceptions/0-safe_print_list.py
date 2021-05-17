cea432bbc931f7865c3ddf7b57fa7765bd14195c
Louvani <paula.louvani@gmail.com>
2021-05-17 13:01:05 -0500

    0x05 task 0 pep8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-	num = 0
-	try:
-		for i in range(0, x):
-			print("{:d}".format(my_list[i]), end="")
-			num += 1
-	except IndexError:
-		pass
-	print("")
-	return num
+    num = 0
+    try:
+        for i in range(0, x):
+            print("{:d}".format(my_list[i]), end="")
+            num += 1
+    except IndexError:
+        pass
+    print("")
+    return num
