26c2fd339f2a33a5c63db8dec165681def2c2ce8
Louvani <paula.louvani@gmail.com>
2021-05-17 14:44:12 -0500

    0x05 task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    num = 0
+    try:
+        num = a / b
+    except ZeroDivisionError:
+        num = None
+    finally:
+        print("Inside result: {}".format(num))
+    return num
