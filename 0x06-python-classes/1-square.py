380b6bce7b1b9b3654a0e532f38798e02f3e722b
Louvani <paula.louvani@gmail.com>
2021-05-18 18:48:47 -0500

    0x06 task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" definition of a square class """
+
+
+class Square:
+    """ defines a square with a private instance attribute"""
+    def __init__(self, size):
+        """size: Square size"""
+        self.__size = size
