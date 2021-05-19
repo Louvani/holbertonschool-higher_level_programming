07027ad97af23d3ab9714f80206969d495590b0b
Louvani <paula.louvani@gmail.com>
2021-05-18 22:25:09 -0500

    0x06 task4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" definition of a square class """
+
+
+class Square:
+    """ defines a square with a private instance attribute"""
+    def __init__(self, size=0):
+        """size: Square size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
