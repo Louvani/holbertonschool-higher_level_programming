82eabb27bfa7be87b400424e0d8d1d7497cfc484
Louvani <paula.louvani@gmail.com>
2021-05-19 08:13:23 -0500

    0x06 task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,13 +6,8 @@ class Square:
     """ defines a square with a private instance attribute"""
     def __init__(self, size=0, position=(0, 0)):
         """size: Square size"""
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         return self.__size ** 2
