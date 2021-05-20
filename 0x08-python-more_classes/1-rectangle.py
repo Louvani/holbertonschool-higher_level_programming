6692e873adaa2bb931ba1dfde06d9c9df6edb184
Louvani <paula.louvani@gmail.com>
2021-05-20 18:21:18 -0500

    0x08 task1 documentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,15 +5,18 @@
 class Rectangle:
     """Class that define a rectangle"""
     def __init__(self, width=0, height=0):
+        """Initialice class Ractangle"""
         self.height = height
         self.width = width
 
     @property
     def width(self):
+        """retrieve width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """set width only if is an integer and if is greather than 0"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
         elif value < 0:
@@ -23,10 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """retrieve height"""
         return self._height
 
     @height.setter
     def height(self, value):
+        """set height only if is an integer and if is greather than 0"""
         if type(value) is not int:
             raise TypeError('height must be an integer')
         elif value < 0:
