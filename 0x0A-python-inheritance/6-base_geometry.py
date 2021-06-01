057c45d4b0ae6df9509bb763dff1c32c63c18a00
Louvani <paula.louvani@gmail.com>
2021-06-01 17:31:24 -0500

    Task6 raise exeption for a non implemented function

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module to test class"""
+
+
+class BaseGeometry:
+    """class BaseGeometry """
+    def area(self):
+        """ raises an Exception """
+        raise Exception('area() is not implemented')
