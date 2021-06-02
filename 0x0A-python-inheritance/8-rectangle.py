75fa54f497442de451ecf426abb48b36de39418c
Louvani <paula.louvani@gmail.com>
2021-06-01 21:21:40 -0500

    Task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module to test class BaseGeometry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry that validated integer and return area """
+    def area(self):
+        """ raises an Exception because is not finished """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """ validates if value is integer """
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+        else:
+            return True
+
+
+class Rectangle(BaseGeometry):
+    """inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        if BaseGeometry.integer_validator(self, "width", width):
+            self._width = width
+        if BaseGeometry.integer_validator(self, "height", height):
+            self._height = height
