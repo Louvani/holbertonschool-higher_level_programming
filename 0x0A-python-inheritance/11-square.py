f17514b748e3e60ff4b06d3b7136e80c1d193d87
Louvani <paula.louvani@gmail.com>
2021-06-01 22:26:38 -0500

    Task11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module to test class BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.__size = size
+        self.integer_validator('size', self.__size)
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Return area of a square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """return the square description"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
