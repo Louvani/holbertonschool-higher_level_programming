4d2fc66c7f785ec5280bcf8456f6192d959bcfc4
Louvani <paula.louvani@gmail.com>
2021-06-02 17:45:42 -0500

    Pep8

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,7 +4,8 @@ JSON representation of an object"""
 
 import json
 
+
 def to_json_string(my_obj):
-	"""function that returns the
-	JSON representation of an object (string)"""
-	return json.dumps(my_obj)
+    """function that returns the
+    JSON representation of an object (string)"""
+    return json.dumps(my_obj)
