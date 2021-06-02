1a72e3eff62ea3b126a50844c4b839d567ecc8d2
Louvani <paula.louvani@gmail.com>
2021-06-01 23:33:23 -0500

    Task 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Doc for module 101"""
+"""Doc for module 101 add atributte"""
 
 
 def add_attribute(obj, variable, value):
-
+    """function that adds a new attribute to an object if it’s possible"""
     type_str = type(obj)
     if "__dict__" in dir(type_str):
         setattr(obj, variable, value)
