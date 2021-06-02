4afae8621adef343d9a18b2c7153f5551b411af2
Louvani <paula.louvani@gmail.com>
2021-06-02 13:28:08 -0500

    Readme and task0 read a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module learn how to read a file"""
+
+
+def read_file(filename=""):
+    """function to read a file and write in the standar output"""
+    with open("my_file_0.txt", encoding='utf-8') as f:
+        print(f.read())
