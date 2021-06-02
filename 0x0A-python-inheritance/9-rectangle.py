46bbe10ed75289045924c0ddc8eaafde84ff9d8e
Louvani <paula.louvani@gmail.com>
2021-06-01 22:06:02 -0500

    task9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module to test class BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """Instantiation with width and height
+        width and height must be private. No getter or setter
+        width and height must be positive integers, validated
+        by integer_validator
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator('width', self.__width)
+        self.integer_validator('height', self.__height)
+
+    def area(self):
+        return self.__width * self.__height
