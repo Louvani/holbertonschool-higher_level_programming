7c0ff7b2686e4f74cd38d3627fbd92c60354622f
Louvani <paula.louvani@gmail.com>
2021-06-05 20:57:13 -0500

    Task 11 getter and setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,8 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """ inherits from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
+        """Class construcctor"""
+        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
@@ -15,3 +17,18 @@ class Square(Rectangle):
         self.x , self.y, self.width, self.height)
         return new_string
 
+    @property
+    def size(self):
+        """retrieve width"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set width only if is an integer and if is greather than 0"""
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        elif value <= 0:
+            raise ValueError('width must be > 0')
+        else:
+            self.width = value
+            self.height = value
