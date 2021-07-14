eef468c6c3a58d313c3a159e88bf467e218dd4ac
Louvani <paula.louvani@gmail.com>
2021-07-13 21:46:55 -0500

    Task7 path for references

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-	id INT NOT NULL UNIQUE,
+	id INT NOT NULL UNIQUE
 	PRIMARY KEY (id) AUTO_INCREMENT,
 	state_id INT NOT NULL,
 	FOREING KEY (state_id), REFERENCES hbtn_0d_usa.states (id),
