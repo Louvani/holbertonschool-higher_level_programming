1deffb0f25e3503c96428d49b88409470c5135bc
Louvani <paula.louvani@gmail.com>
2021-07-13 21:41:56 -0500

    Task7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL UNIQUE,
+PRIMARY KEY (id) AUTO_INCREMENT,
+state_id INT NOT NULL,
+FOREING KEY (state_id), REFERENCES states (id),
+name VARCHAR(256) NOT NULL);
