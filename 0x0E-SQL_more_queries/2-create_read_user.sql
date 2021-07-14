ae5ef6d45dc7c1a33bf0bef8057dffb8a3ffaec9
Louvani <paula.louvani@gmail.com>
2021-07-13 20:23:46 -0500

    Task2 two comments

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2.
+-- joda
+CREATE DATABASE IF NOT EXISTS hbtn_0c_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
