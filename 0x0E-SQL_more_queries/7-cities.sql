23d81cff1a9b31b73cf1b85e838ae0b171037e54
Louvani <paula.louvani@gmail.com>
2021-07-13 21:49:57 -0500

    Task7 no moreee

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,9 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-	id INT NOT NULL UNIQUE,
-	PRIMARY KEY (id) AUTO_INCREMENT,
+	id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
 	state_id INT NOT NULL,
-	FOREING KEY (state_id) REFERENCES hbtn_0d_usa.states (id),
+	FOREING KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
 	name VARCHAR(256) NOT NULL
 );
