f0d4f7ed036ffbbcd64f76fb16fe7ac43e78eaa2
Louvani <paula.louvani@gmail.com>
2021-07-13 20:55:00 -0500

    Task4 default

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- creates the table id_not_null on your MySQL server.
-CREATE TABLE if not exists id_not_null (id INT 1, name VARCHAR(256));
+CREATE TABLE if not exists id_not_null (id INT DEFAULT 1, name VARCHAR(256));
