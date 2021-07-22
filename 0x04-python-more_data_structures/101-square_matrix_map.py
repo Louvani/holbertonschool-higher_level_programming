cb5316d051dc37468e3a5b9f0c9431732494fcc2
Louvani <paula.louvani@gmail.com>
2021-07-22 12:05:54 -0500

    project 0x04 task 14. file should be max 3 lines

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-'''
-function that computes the square value
-of all integers of a matrix using map
-'''
-
-
 def square_matrix_map(matrix=[]):
-    new = list(map(lambda i: list(map(lambda item: item ** 2, i)), matrix))
-    return new
+    return list(map(lambda i: list(map(lambda item: item ** 2, i)), matrix))
