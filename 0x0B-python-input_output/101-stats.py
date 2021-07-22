d740aeb8e1aaa34369e4660a7d41fe18d0d74dbd
Louvani <paula.louvani@gmail.com>
2021-07-22 17:34:27 -0500

    project 0x0B-python-input_output: task 14. Log parsing

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+file_size = 0
+status_code = []
+status_code_list = ["200", "301", "400", "401", "403", "404", "405", "500"]
+
+for idx, line in enumerate(sys.stdin):
+    words = line.split()
+    file_size += int(words[-1])
+    status_code.append(words[-2])
+    if not idx % 10 and idx != 0:
+        print('File size: {:d}'.format(file_size))
+        file_size = 0
+        for key in status_code_list:
+            if key in status_code:
+                print("{}: {:d}".format(key, status_code.count(key)))
+        status_code = []
