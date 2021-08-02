14de1a750f471062c12d8d08931d4b7261fe27be
Louvani <paula.louvani@gmail.com>
2021-08-02 11:13:56 -0500

    Add task 19 recursion to get factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function factorial (a) {
+  if (a === 0) {
+    return -1;
+  }
+  if (a === 1) {
+    return 1;
+  }
+  return a * factorial(a -1);
+}
+const num1 = Number.parseInt(process.argv[2]);
+if (isNaN(num1)) {
+  console.log(1);
+} else {
+  const result = factorial(num1, num1 - 1);
+  console.log(result);
+}
