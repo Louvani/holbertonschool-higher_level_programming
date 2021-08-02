e1d1710d4df730b829105a07ae6b443f59da1c29
Louvani <paula.louvani@gmail.com>
2021-08-02 12:16:06 -0500

    Add task 11 Second biggest!

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+if (!process.argv[2]) {
+  console.log(0);
+} else if (!process.argv[3]) {
+  console.log(0);
+} else {
+  const list1 = [];
+  let idx = 2;
+  while (idx < process.argv.length) {
+    list1.push(Number.parseInt(process.argv[idx]));
+    idx += 1;
+  }
+  list1.sort();
+  for (idx = list1.length - 2; idx >= 0; idx--) {
+    if (list1[idx] !== list1[list1.length - 1]) {
+      console.log(list1[idx]);
+      break;
+    }
+  }
+}
