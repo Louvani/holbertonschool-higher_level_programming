3fff2ad37d67bfd15544d61e592b513327156a69
Louvani <paula.louvani@gmail.com>
2021-08-02 11:28:26 -0500

    style to task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -7,7 +7,7 @@ function factorial (a) {
   if (a === 1) {
     return 1;
   }
-  return a * factorial(a -1);
+  return a * factorial(a - 1);
 }
 const num1 = Number.parseInt(process.argv[2]);
 if (isNaN(num1)) {
