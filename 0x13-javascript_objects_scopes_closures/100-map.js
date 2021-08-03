3fd8f6642a404e4ffb9c4be197ab448c771ba62e
Louvani <paula.louvani@gmail.com>
2021-08-03 18:19:01 -0500

    Task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,5 +2,5 @@
 
 const firstList = require('./100-data').list;
 console.log(firstList);
-const map1 = firstList.map(x => x * (x - 1));
+const map1 = firstList.map((x, index) => x * index);
 console.log(map1);
