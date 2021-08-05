120d14a607f4244f15b84c769561e7478e3cad91
Louvani <paula.louvani@gmail.com>
2021-08-05 11:51:20 -0500

    Update task 1 condition as query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,10 +5,9 @@ conn = MySQLdb.connect(host="localhost", port=3306, user="root",
                             passwd="root", db="hbtn_0e_0_usa", charset="utf8")
 cur = conn.cursor()
 # HERE I have to know SQL to grab all states in my database
-cur.execute("SELECT * FROM states ORDER BY id ASC")
+cur.execute("SELECT * FROM states WHERE name regexp '^N.' ORDER BY id ASC")
 query_rows = cur.fetchall()
 for row in query_rows:
-    if row[1][0] == 'N':
         print(row)
 cur.close()
 conn.close()
