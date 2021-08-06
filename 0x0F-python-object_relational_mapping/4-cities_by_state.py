e91243823a555ea03078b8701c8398f18d033589
Louvani <paula.louvani@gmail.com>
2021-08-06 10:18:57 -0500

    Update task 4 inner join

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,10 @@ conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                             passwd=argv[2], db=argv[3], charset="utf8")
 cur = conn.cursor()
 # HERE I have to know SQL to grab all states in my database
-cur.execute("SELECT * FROM cities ORDER BY id ASC")
+cur.execute("SELECT cities.id, cities.name, states.name\
+        FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                        ORDER BY cities.id ASC")
 query_rows = cur.fetchall()
 for row in query_rows:
         print(row)
