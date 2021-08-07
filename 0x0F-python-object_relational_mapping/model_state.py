ee2596a9c37440768fe3ff75b80eb67b1df58951
Louvani <paula.louvani@gmail.com>
2021-08-06 21:33:13 -0500

    Null able in task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,4 +13,4 @@ class State(Base):
     '''Class State'''
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable == False)
+    name = Column(String(128), nullable=False)
