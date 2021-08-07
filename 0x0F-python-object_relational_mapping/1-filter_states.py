0e1b14d57ace7ce81ae40fced496f18b184432d9
Louvani <paula.louvani@gmail.com>
2021-08-07 08:30:26 -0500

    Update task1 importing sys

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,6 +3,8 @@
 from the database hbtn_0e_0_usa'''
 
 import MySQLdb
+from sys import argv
+
 if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                                 passwd=argv[2], db=argv[3], charset="utf8")
