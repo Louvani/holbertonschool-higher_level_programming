0602c1cbef26192bd342dea44a16530981cbbf5e
Louvani <paula.louvani@gmail.com>
2021-08-07 08:34:32 -0500

    Update task 0 connection without charset

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-'''Module to select states from hbtn_0e_0_usa DataBase'''
+'''
+Module to select states from hbtn_0e_0_usa DataBase
+'''
+
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
+
     connection = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                                 passwd=argv[2], db=argv[3], charset="utf8")
+                                 passwd=argv[2], db=argv[3])
     cur = connection.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
