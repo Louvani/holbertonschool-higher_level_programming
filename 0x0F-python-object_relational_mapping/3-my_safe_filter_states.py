91f4d3523ca7c48464686145701a70e1860274ff
Louvani <paula.louvani@gmail.com>
2021-08-07 09:17:41 -0500

    Update task3 WHERE BINARY

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,15 +5,20 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                                passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    # HERE I have to know SQL to grab all states in my database
-    sql = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+    connect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                              passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = connect.cursor()
+
+    query = "SELECT * FROM states\
+        WHERE BINARY name = %s\
+        ORDER BY id ASC"
     name = (argv[4], )
-    cur.execute(sql, name)
-    query_rows = cur.fetchall()
+    cursor.execute(query, name)
+
+    query_rows = cursor.fetchall()
     for row in query_rows:
         print(row)
-    cur.close()
-    conn.close()
+
+    cursor.close()
+    connect.close()
