1782938e7de7586648c98f640c6c951674e8824c
Louvani <paula.louvani@gmail.com>
2021-08-07 08:53:22 -0500

    Remove cursor.close()

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-'''Module to  lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa'''
+'''
+Module to  lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+'''
 
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                                passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    # HERE I have to know SQL to grab all states in my database
-    cur.execute("SELECT * FROM states WHERE name regexp '^N.' ORDER BY id ASC")
-    query_rows = cur.fetchall()
+
+    connect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                              passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = connect.cursor()
+    query = "SELECT * FROM states\
+        WHERE name regexp '^N.'\
+        ORDER BY id ASC"
+    cursor.execute(query)
+    query_rows = cursor.fetchall()
     for row in query_rows:
         print(row)
-    cur.close()
-    conn.close()
+    connect.close()
