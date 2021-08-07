097d493b5fc717155cc6fda0edc3c536e1f3a9b3
Louvani <paula.louvani@gmail.com>
2021-08-07 09:12:09 -0500

    Update task2 format {} without ':s'

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-'''Module that takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument. '''
+'''
+Module that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
+'''
+
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                                passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
-    # HERE I have to know SQL to grab all states in my database
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}'\
-        ORDER BY id ASC".format(argv[4]))
-    query_rows = cur.fetchall()
+
+    connect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                              passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = connect.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE '{}'\
+        ORDER BY id ASC".format(argv[4])
+
+    cursor.execute(query)
+
+    query_rows = cursor.fetchall()
     for row in query_rows:
         print(row)
-    cur.close()
-    conn.close()
+    cursor.close()
+    connect.close()
