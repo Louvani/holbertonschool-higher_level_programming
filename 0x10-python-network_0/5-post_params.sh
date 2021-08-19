01b439990d1d9ef04efeb96c6377e33d95495dc5
Louvani <paula.louvani@gmail.com>
2021-08-19 17:05:25 -0500

    Task5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# 5. cURL POST parameters 
-curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+# 5. cURL POST parameters
+curl -s "$1" | grep "HTTP/" | awk '{print $2}'
