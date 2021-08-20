acf235f0ee77076e6782c96653205ac6a368e5cf
Louvani <paula.louvani@gmail.com>
2021-08-20 14:23:44 -0500

    Task 3 fix print in error

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
         with urllib.request.urlopen(req) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.URLError as e:
-        print(e.code)
+        print("Error code: {}".format(e.code))
