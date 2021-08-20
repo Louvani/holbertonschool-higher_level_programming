a922c9348cf8cd072f7271e0ea14085ef8f4c412
Louvani <paula.louvani@gmail.com>
2021-08-20 14:47:22 -0500

    Only impor request in task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
 """3. Error code #0"""
 
-from sys import argv
 import urllib.request
-import urllib.error
 
 if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
 
-    try:
-        req = urllib.request.Request(url)
-        with urllib.request.urlopen(req) as response:
-            the_page = response.read().decode("utf-8")
-        print("Body response:")
-        print("\t- type: {}".format(type(the_page)))
-        print("\t- content: {}".format(the_page))
-    except urllib.error.URLError as e:
-        print("Error code: {}".format(e.code))
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read().decode("utf-8")
+    print("Body response:")
+    print("\t- type: {}".format(type(the_page)))
+    print("\t- content: {}".format(the_page))
