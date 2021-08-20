2f020dae8b341f08d82e0e17d8141551c6fce948
Louvani <paula.louvani@gmail.com>
2021-08-20 14:42:27 -0500

    Task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""3. Error code #0"""
+
+from sys import argv
+import urllib.request
+import urllib.error
+
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+
+    try:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            the_page = response.read().decode("utf-8")
+        print("Body response:")
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- utf8 content: {}".format(the_page))
+    except urllib.error.URLError as e:
+        print("Error code: {}".format(e.code))
