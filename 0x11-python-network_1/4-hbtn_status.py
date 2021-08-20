ecd1877c29d7be6a0089fd789d77db65025d4641
Louvani <paula.louvani@gmail.com>
2021-08-20 15:02:00 -0500

    I was wrong with the library

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """3. Error code #0"""
 
-import urllib.request
+import requests
 
 if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
+    r = requests.get('https://intranet.hbtn.io/status')
 
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read().decode("utf-8")
     print("Body response:")
-    print("\t- type: {}".format(type(the_page)))
-    print("\t- content: {}".format(the_page))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
