ab8fd6d454e66332df85a1bc003f73a547f91b08
Louvani <paula.louvani@gmail.com>
2021-08-20 18:43:21 -0500

    Advanced Task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""10. Time for an interview!"""
+
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+    owner = argv[1]
+    repository = argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(
+        owner, repository)
+    values = {'sort': 'created', 'per_page': 10}
+    req = requests.get(url, params=values)
+    try:
+        response = req.json()
+        if len(response) > 0:
+            for item in response:
+                print("{}: {}".format(
+                    item['sha'], item['commit']['author']['name']))
+        else:
+            print("No result")
+    except:
+        print("None")
