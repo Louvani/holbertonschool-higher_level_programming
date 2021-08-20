8f05ce5b7e5f20b599e0f518bdbf0d8e8ff91a5f
Louvani <paula.louvani@gmail.com>
2021-08-20 14:21:10 -0500

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""3. Error code #0"""
+
+from sys import argv
+import urllib.request
+import urllib.error
+
+if __name__ == '__main__':
+    url = argv[1]
+
+    try:
+        req = urllib.request.Request(url)
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.URLError as e:
+        print(e.code)
