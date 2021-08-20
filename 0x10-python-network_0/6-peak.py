78a1a629ef58f4eb1a8f4738719ad02a5a4ff099
Louvani <paula.louvani@gmail.com>
2021-08-19 23:17:30 -0500

    add documetation

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,13 +3,14 @@
 
 
 def find_peak(list_of_integers):
-
+    """send to recursion"""
     n = len(list_of_integers)
     if n > 1:
         return recursion_find_peak(list_of_integers, 0, n - 1, n)
 
 
 def recursion_find_peak(arr, low, high, n):
+    """Find the peak"""
     center = int(low + (high - low)/2)
 
     if ((center == 0 or arr[center - 1] <= arr[center]) and
