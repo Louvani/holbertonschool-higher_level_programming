28c4b1511eb4af8fc97fe0c3b96f754b8fcfd500
Louvani <paula.louvani@gmail.com>
2021-08-19 20:14:06 -0500

    Last task with permision

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 9
-curl -s 0.0.0.0:5000/catch_me -Li -X PUT -d "user_id=98" -H "origin: HolbertonSchool"
+curl -s 0.0.0.0:5000/catch_me -L -X PUT -d "user_id=98" -H "origin: HolbertonSchool"
