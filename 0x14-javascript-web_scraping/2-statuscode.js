3771c439d74cb51a64381411d0360abbf8b389f2
Louvani <paula.louvani@gmail.com>
2021-09-14 15:11:51 -0500

    2. Status code, correct string in output

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ const request = require('request');
 const url = process.argv[2];
 
 request.get(url).on('response', function (response) {
-  console.log(response.statusCode);
+  console.log('code: ' + response.statusCode);
 });
