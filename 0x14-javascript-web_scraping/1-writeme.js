c954c278a8fd9eb6e06ce0c43424911ae474d452
Louvani <paula.louvani@gmail.com>
2021-09-14 14:25:08 -0500

    1. Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(file, content, err => {
+  if (err) {
+    console.error(err);
+  }
+});
